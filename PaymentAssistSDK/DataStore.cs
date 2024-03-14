85010b1541bffdde4633e740b4a19233f692a521
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2024-03-14 09:56:57 +0000

    fix hypothetical null dereference

--- PaymentAssistSDK/DataStore.cs
+++ PaymentAssistSDK/DataStore.cs
@@ -6,8 +6,14 @@ internal static class DataStore
     public static string APISecret = "";
     public static string APIURL 
     {
-        get => _apiURL.Length == 0 || _apiURL.Last() == '/' ? _apiURL : _apiURL + "/";
-        set => _apiURL = value;
+        get => _apiURL;
+        set 
+        {
+            if (string.IsNullOrEmpty(value))
+                _apiURL = "";
+            else
+                _apiURL = value.Last() == '/' ? value : value + "/";
+        }
     }
 
     private static string _apiURL = "";
